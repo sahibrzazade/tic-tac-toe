a90a6ceaeb6e49b79a76e4e46482b21fb8286e20
sahibrzazade <sahibrzazade03@gmail.com>
2024-12-19 23:41:25 +0400

    Responsive issue fixed

--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -4,11 +4,11 @@ const Gameboard = () => {
   const { boxes, player1, player2, isPlaying, handleClick } =
     useGlobalProvider();
   return (
-    <div className="h-[420px] w-[420px] flex flex-wrap my-8">
+    <div className="h-[240px] w-[240px] md:h-[420px] md:w-[420px] flex flex-wrap my-8">
       {boxes.map((x, id) => (
         <button
           key={id}
-          className={`box w-[140px] h-[140px] border-none text-[5rem] flex items-center justify-center font-light bg-slate-900 transition-all ${
+          className={`box h-[80px] w-[80px] md:h-[140px] md:w-[140px] border-none text-6xl md:text-8xl flex items-center justify-center font-light bg-slate-900 transition-all ${
             x.player || isPlaying !== true
               ? "hover:cursor-default hover:bg-slate-900"
               : "hover:cursor-pointer hover:bg-slate-800"
